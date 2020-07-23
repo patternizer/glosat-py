7a2a1572f2c9db1cf40fd64b6e9c575479c0e9f1
patternizer <patternizer@gmail.com>
2020-07-23 18:01:46 +0100

    Use colormap from cmocean (https://matplotlib.org/cmocean/) for multiyear climatology.

--- app.py
+++ app.py
@@ -15,16 +15,19 @@
 #------------------------------------------------------------------------------
 import numpy as np
 import numpy.ma as ma
+from mod import Mod
 import pandas as pd
 import xarray as xr
 # Plotting libraries:
 import matplotlib
 import matplotlib.pyplot as plt; plt.close('all')
 import matplotlib.cm as cm
+import matplotlib.colors as c
 from matplotlib import colors as mcol
 from matplotlib.cm import ScalarMappable
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
+#matplotlib.rcParams['text.usetex'] = True
 import matplotlib.dates as mdates
 import matplotlib.colors as mcolors
 import matplotlib.ticker as mticker
@@ -32,6 +35,8 @@ from matplotlib.collections import PolyCollection
 from mpl_toolkits import mplot3d
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d import proj3d
+#import seaborn as sns
+import cmocean
 # Mapping libraries:
 import cartopy
 import cartopy.crs as ccrs
@@ -200,14 +205,21 @@ def update_plot_worldmap(value, colors):
     elif colors == 'Magma': cmap = px.colors.sequential.Magma_r
     elif colors == 'Coolwarm': cmap = px.colors.sequential.RdBu_r
     
+    Y = df[df['stationcode']==stationcode[value]].iloc[:,range(1,13)].T
+    n = len(Y.T)            
+    colors = cmocean.cm.balance(np.linspace(0.05,0.95,n)) # array([[0.13774403, 0.17485525, 0.41001322, 1.], [...])
+    hexcolors = [ "#{:02x}{:02x}{:02x}".format(int(colors[i][0]*255),int(colors[i][1]*255),int(colors[i][2]*255)) for i in range(len(colors)) ]
+    cmap = hexcolors
+
     lat = [df[df['stationcode']==stationcode[value]]['stationlat'].iloc[0]]
     lon = [-df[df['stationcode']==stationcode[value]]['stationlon'].iloc[0]]
     var = [df[df['stationcode']==stationcode[value]].iloc[:,1:13].mean().mean()]
     station = df[df['stationcode']==stationcode[value]]['stationinfo'].iloc[0]
     
-    fig = go.Figure(go.Densitymapbox(lat=lat, lon=lon, z=var, zmin=-2, zmax=2, radius=10, colorscale=cmap, autocolorscale = False))
+#    fig = go.Figure(go.Densitymapbox(lat=lat, lon=lon, z=var, zmin=-2, zmax=2, radius=10, colorscale=cmap, autocolorscale = False))
+    fig = go.Figure(go.Densitymapbox(lat=lat, lon=lon, z=var, radius=10, colorscale=cmap, autocolorscale = False))
     fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=0) 
-    fig.update_layout(title={'text': str(station), 'x':0.5, 'y':0.95, 'xanchor': 'center', 'yanchor': 'top'})
+    fig.update_layout(title={'text': str(station), 'x':0.5, 'y':0.95, 'xanchor': 'left', 'yanchor': 'top'})
     fig.update_layout(margin={"r":0,"t":50,"l":10,"b":0})
     
     return fig
@@ -272,13 +284,17 @@ def update_plot_climatology(value, colors):
 
     X = df[df['stationcode']==stationcode[value]].iloc[:,0]
     Y = df[df['stationcode']==stationcode[value]].iloc[:,range(1,13)].T
-    
+
+    n = len(Y.T)            
+    colors = cmocean.cm.balance(np.linspace(0.05,0.95,n)) # array([[0.13774403, 0.17485525, 0.41001322, 1.], [...])
+    hexcolors = [ "#{:02x}{:02x}{:02x}".format(int(colors[i][0]*255),int(colors[i][1]*255),int(colors[i][2]*255)) for i in range(len(colors)) ]
+
     data = []
     for k in range(len(Y.T)):
         trace=[go.Scatter(              
             x=np.arange(1,13), y=Y.iloc[:,k], 
             mode='lines+markers', 
-            line=dict(width=1.0),
+            line=dict(width=1.5, color=hexcolors[k]),
             marker=dict(size=5, opacity=0.5),
             name=str(X.iloc[k]))]
         data = data + trace
@@ -292,7 +308,6 @@ def update_plot_climatology(value, colors):
     
     return fig
 
-                    
 ##################################################################################################
 # Run the dash app
 ##################################################################################################
