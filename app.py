eee86202e2c0cb46755ef727dfd2e805a0b37ca8
patternizer <patternizer@gmail.com>
2020-07-24 01:41:01 +0100

    Interpolated colorscale for seasonal cycles. Added yearly means to timeseries. Added stationinfo header extract.

--- app.py
+++ app.py
@@ -3,8 +3,8 @@
 #------------------------------------------------------------------------------
 # PROGRAM: app.py
 #------------------------------------------------------------------------------
-# Version 0.2
-# 22 July, 2020
+# Version 0.3
+# 23 July, 2020
 # Michael Taylor
 # https://patternizer.github.io
 # patternizer AT gmail DOT com
@@ -27,7 +27,7 @@ from matplotlib import colors as mcol
 from matplotlib.cm import ScalarMappable
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
-#matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams['text.usetex'] = True
 import matplotlib.dates as mdates
 import matplotlib.colors as mcolors
 import matplotlib.ticker as mticker
@@ -61,7 +61,6 @@ import random
 from random import randint
 from random import randrange
 
-
 # Silence library version notifications
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -77,10 +76,6 @@ stationlon = -df['stationlon']
 stationlat = df['stationlat']
 stationcode = df['stationcode'].unique()
 
-projection = 'platecarree'
-
-#monthstr = ['Jan', 'Feb', 'Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-value = '10080'
 opts = [{'label' : stationcode[i], 'value' : i} for i in range(len(stationcode))]
 
 # ========================================================================
@@ -110,28 +105,35 @@ app.layout = html.Div(children=[
                 dcc.Dropdown(
                     id = "station",
                     options = opts,           
-                    value = 0,
-                    style = {'padding' : '10px', 'width': '60%', 'display': 'inline-block'},
+                    value = 10080,
+                    style = {'padding' : '10px', 'width': '100%', 'display': 'inline-block'},
                 ),                                    
             ]), 
-            width={'size':8}, 
-            ),                        
-            dbc.Col(html.Div([
-                dcc.RadioItems(
-                    id = "colormap",
-                    options=[
-                        {'label': ' Viridis', 'value': 'Viridis'},
-                        {'label': ' Cividis', 'value': 'Cividis'},
-                        {'label': ' Plotly3', 'value': 'Plotly3'},
-                        {'label': ' Magma', 'value': 'Magma'},
-                        {'label': ' Coolwarm', 'value': 'Coolwarm'}                        
-                    ],
-                    value = 'Coolwarm',
-                    labelStyle={'padding' : '5px', 'display': 'inline-block'},
-                ),
-            ]), 
+            width={'size':2}, 
+            ),         
+               
+            dbc.Col(
+
+            html.Div([
+                dcc.Graph(id="station-info"),
+                html.Br(),
+#                html.Label(['Dataset: ', html.A('CRUTEM5.1 v=prelim01', href='https://catalogue.ceda.ac.uk/uuid/eeabb5e1ff2140f48e76ea1ffda6bb48'), ' by ', html.A('UEA-CRU, UEA-NCAS, MO-HC', href='https://crudata.uea.ac.uk/cru/data/temperature/')]),            
+#                html.Label(['Dataviz: ', html.A('Github', href='https://github.com/patternizer/glosat-py'), ' by ', html.A('patternizer', href='https://patternizer.github.io')]),            
+                html.Label(['Dataset: ', html.A('CRUTEM5.1 v=prelim01 ', href='https://catalogue.ceda.ac.uk/uuid/eeabb5e1ff2140f48e76ea1ffda6bb48')]),
+                html.Br(),
+                html.Label(['Dataviz: ', html.A('Github', href='https://github.com/patternizer/glosat-py')]),            
+            ],
+            style = {'padding' : '10px', 'width': '100%', 'display': 'inline-block'}),    
+
             width={'size':4}, 
             ),
+
+            dbc.Col(html.Div([
+                dcc.Graph(id="plot-worldmap", style = {'padding' : '10px', 'width': '100%', 'display': 'inline-block'}),                                 
+            ]), 
+            width={'size':6}, 
+            ),            
+  
         ]),
     ]),
 # ------------
@@ -143,43 +145,15 @@ app.layout = html.Div(children=[
             dbc.Col(html.Div([                    
                 dcc.Graph(id="plot-timeseries", style = {'padding' : '10px', 'width': '100%', 'display': 'inline-block'}),                                      
             ]), 
-            width={'size':4}, 
+            width={'size':6}, 
             ),                        
             dbc.Col(html.Div([
                 dcc.Graph(id="plot-climatology", style = {'padding' : '10px', 'width': '100%', 'display': 'inline-block'}),                                 
             ]), 
-            width={'size':4}, 
-            ),            
-            dbc.Col(html.Div([
-                dcc.Graph(id="plot-worldmap", style = {'padding' : '10px', 'width': '100%', 'display': 'inline-block'}),                                 
-            ]), 
-            width={'size':4}, 
+            width={'size':6}, 
             ),            
         ]),
     ]),
-# ------------
-
-# ------------
-    html.Div([   
-        dbc.Row([
-
-            dbc.Col(html.Div([                    
-
-                html.P([
-                    html.H3(children='About'),
-                    html.Label('A visual tool for inspecting the CRUTEM5.1 dataset'),
-                    html.Br(),    
-                    html.Label(['Codebase: ', html.A('Github', href='https://github.com/patternizer/glosat-py')]),       
-                    html.Br(),                  
-                    html.Label(['Created using Plotly Python by ', html.A('Michael Taylor', href='https://patternizer.github.io')]),            
-                ],                        
-                style = {'padding' : '10px', 'width': '100%', 'display': 'inline-block'}),                                      
-            ]), 
-            width={'size':12}, 
-            ),                        
-
-        ]),                        
-    ]), 
 # ------------
 
 ])
@@ -189,104 +163,118 @@ app.layout = html.Div(children=[
 # ========================================================================
 
 @app.callback(
-    Output(component_id='plot-worldmap', component_property='figure'),
-    [Input(component_id='station', component_property='value'), 
-    Input(component_id='colormap', component_property='value')],    
+    Output(component_id='station-info', component_property='figure'),
+    [Input(component_id='station', component_property='value')],    
     )
     
-def update_plot_worldmap(value, colors):
+def update_station_info(value):
     
     """
-    Plot station location on world map
+    Display station info
     """
-    if colors == 'Viridis': cmap = px.colors.sequential.Viridis_r
-    elif colors == 'Cividis': cmap = px.colors.sequential.Cividis_r
-    elif colors == 'Plotly3': cmap = px.colors.sequential.Plotly3_r
-    elif colors == 'Magma': cmap = px.colors.sequential.Magma_r
-    elif colors == 'Coolwarm': cmap = px.colors.sequential.RdBu_r
-    
-    Y = df[df['stationcode']==stationcode[value]].iloc[:,range(1,13)].T
-    n = len(Y.T)            
-    colors = cmocean.cm.balance(np.linspace(0.05,0.95,n)) # array([[0.13774403, 0.17485525, 0.41001322, 1.], [...])
-    hexcolors = [ "#{:02x}{:02x}{:02x}".format(int(colors[i][0]*255),int(colors[i][1]*255),int(colors[i][2]*255)) for i in range(len(colors)) ]
-    cmap = hexcolors
 
-    lat = [df[df['stationcode']==stationcode[value]]['stationlat'].iloc[0]]
-    lon = [-df[df['stationcode']==stationcode[value]]['stationlon'].iloc[0]]
-    var = [df[df['stationcode']==stationcode[value]].iloc[:,1:13].mean().mean()]
+    code = df[df['stationcode']==stationcode[value]]['stationcode'].iloc[0]
     station = df[df['stationcode']==stationcode[value]]['stationinfo'].iloc[0]
-    
-#    fig = go.Figure(go.Densitymapbox(lat=lat, lon=lon, z=var, zmin=-2, zmax=2, radius=10, colorscale=cmap, autocolorscale = False))
-    fig = go.Figure(go.Densitymapbox(lat=lat, lon=lon, z=var, radius=10, colorscale=cmap, autocolorscale = False))
-    fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=0) 
-    fig.update_layout(title={'text': str(station), 'x':0.5, 'y':0.95, 'xanchor': 'left', 'yanchor': 'top'})
-    fig.update_layout(margin={"r":0,"t":50,"l":10,"b":0})
-    
-    return fig
+                                  
+    data = [
+        go.Table(
+            header=dict(values=['Station','Value'],
+                line_color='darkslategray',
+                fill_color='lightgrey',
+                align='left'),
+            cells=dict(values=[[str(code)], # 1st column
+                [str(station)]], # 2nd column
+                line_color='darkslategray',
+                fill_color='white',
+                align='left')
+        ),
+    ]
+    layout = go.Layout(  height=140, width=380, margin=dict(r=10, l=0, b=0, t=0))
+
+    return {'data': data, 'layout':layout} 
 
 @app.callback(
     Output(component_id='plot-timeseries', component_property='figure'),
-    [Input(component_id='station', component_property='value'), 
-    Input(component_id='colormap', component_property='value')],    
+    [Input(component_id='station', component_property='value')],    
     )
     
-def update_plot_timeseries(value, colors):
+def update_plot_timeseries(value):
     
     """
     Plot station timeseries
     """
-    if colors == 'Viridis': cmap = px.colors.sequential.Viridis_r
-    elif colors == 'Cividis': cmap = px.colors.sequential.Cividis_r
-    elif colors == 'Plotly3': cmap = px.colors.sequential.Plotly3_r
-    elif colors == 'Magma': cmap = px.colors.sequential.Magma_r
-    elif colors == 'Coolwarm': cmap = px.colors.sequential.RdBu_r
 
     da = df[df['stationcode']==stationcode[value]].iloc[:,range(0,13)]
     ts = []    
     for i in range(len(da)):            
         monthly = da.iloc[i,1:]
         ts = ts + monthly.to_list()    
-    ts = np.array(ts)                    
-    t = pd.date_range(start=str(da['year'].iloc[0]), periods=len(ts), freq='M')            
-    ts_yearly = da.groupby(da['year']).mean().values
-    t_yearly = pd.date_range(start=str(da['year'].iloc[0]), periods=len(ts_yearly), freq='Y')    
+    ts = np.array(ts)                
+    ts_yearly = []    
+    ts_yearly_sd = []    
+    for i in range(len(da)):            
+        yearly = np.nanmean(da.iloc[i,1:])
+        yearly_sd = np.nanstd(da.iloc[i,1:])
+        ts_yearly.append(yearly)    
+        ts_yearly_sd.append(yearly_sd)    
+    ts_yearly_sd = np.array(ts_yearly_sd)                    
+    t = pd.date_range(start=str(da['year'].iloc[0]), periods=len(ts), freq='M')     
+    t_yearly = pd.date_range(start=str(da['year'].iloc[0]), periods=len(ts_yearly), freq='A')   
+
+    Y = df[df['stationcode']==stationcode[value]].iloc[:,range(1,13)].T
+    n = len(Y.T)            
+    colors = cmocean.cm.balance(np.linspace(0.05,0.95,n)) 
+    hexcolors = [ "#{:02x}{:02x}{:02x}".format(int(colors[i][0]*255),int(colors[i][1]*255),int(colors[i][2]*255)) for i in range(len(colors)) ]
                                   
-    fig = go.Figure(
+    data=[
         go.Scatter(x=t, y=ts, 
             mode='lines+markers', 
-            line=dict(width=1.0, color='navy'),
-            marker=dict(size=5, opacity=0.5),
+            legendgroup="a",
+            line=dict(width=1.0, color='lightgrey'),
+            marker=dict(size=5, opacity=0.5, color='grey'),
+            name='Monthly',
+            yaxis='y1',
+            error_y=dict(
+                type='constant',
+                array=ts_yearly_sd,
+                visible=False),
+        ),
+        go.Scatter(x=t_yearly, y=ts_yearly, 
+            mode='markers', 
+            legendgroup="a",
+#            line=dict(width=2.0, color=hexcolors),
+            marker=dict(size=10, opacity=1.0, color=hexcolors),
+            name='Yearly',
+            yaxis='y1',
         )
-    )   
+    ]   
+    
+    fig = go.Figure(data)
     fig.update_layout(
-#        xlabel("Year"),
-#        ylabel("Monthly temperature anomaly, $\mathrm{\degree}C$"),
-        title={'text': 'Monthly temperature anomaly timeseries', 'x':0.5, 'y':0.95, 'xanchor': 'center', 'yanchor': 'top'})
-    fig.update_layout(margin={"r":0,"t":50,"l":10,"b":0})
+        xaxis_title = {'text': 'Year'},
+        yaxis_title = {'text': 'Temperature anomaly, °C'},
+#        title = {'text': 'Seasonal cycle', 'x':0.5, 'y':0.925, 'xanchor': 'center', 'yanchor': 'top'}
+    )
+    fig.update_layout(height=300, width=700, margin={"r":0,"t":0,"l":10,"b":0})    
 
     return fig
 
 @app.callback(
     Output(component_id='plot-climatology', component_property='figure'),
-    [Input(component_id='station', component_property='value'), 
-    Input(component_id='colormap', component_property='value')],    
+    [Input(component_id='station', component_property='value')],    
     )
-def update_plot_climatology(value, colors):
+
+def update_plot_climatology(value):
     
     """
     Plot station climatology
     """
-    if colors == 'Viridis': cmap = px.colors.sequential.Viridis_r
-    elif colors == 'Cividis': cmap = px.colors.sequential.Cividis_r
-    elif colors == 'Plotly3': cmap = px.colors.sequential.Plotly3_r
-    elif colors == 'Magma': cmap = px.colors.sequential.Magma_r
-    elif colors == 'Coolwarm': cmap = px.colors.sequential.RdBu_r
 
     X = df[df['stationcode']==stationcode[value]].iloc[:,0]
     Y = df[df['stationcode']==stationcode[value]].iloc[:,range(1,13)].T
 
     n = len(Y.T)            
-    colors = cmocean.cm.balance(np.linspace(0.05,0.95,n)) # array([[0.13774403, 0.17485525, 0.41001322, 1.], [...])
+    colors = cmocean.cm.balance(np.linspace(0.05,0.95,n)) 
     hexcolors = [ "#{:02x}{:02x}{:02x}".format(int(colors[i][0]*255),int(colors[i][1]*255),int(colors[i][2]*255)) for i in range(len(colors)) ]
 
     data = []
@@ -301,10 +289,44 @@ def update_plot_climatology(value, colors):
 
     fig = go.Figure(data)
     fig.update_layout(
-#        xlabel("Month"),
-#        ylabel("Monthly temperature anomaly, $\mathrm{\degree}C$")        
-        title={'text': 'Monthly temperature anomaly seasonal cycle', 'x':0.5, 'y':0.95, 'xanchor': 'center', 'yanchor': 'top'})
-    fig.update_layout(margin={"r":0,"t":50,"l":10,"b":0})
+        xaxis_title = {'text': 'Month'},
+        yaxis_title = {'text': 'Monthly temperature anomaly, °C'},
+#        title = {'text': 'Seasonal cycle', 'x':0.5, 'y':0.925, 'xanchor': 'center', 'yanchor': 'top'}
+    )
+    fig.update_layout(height=300, width=600, margin={"r":0,"t":0,"l":10,"b":0})
+    
+    return fig
+
+@app.callback(
+    Output(component_id='plot-worldmap', component_property='figure'),
+    [Input(component_id='station', component_property='value')])
+    
+def update_plot_worldmap(value):
+    
+    """
+    Plot station location on world map
+    """
+
+    Y = df[df['stationcode']==stationcode[value]].iloc[:,range(1,13)].T
+    n = len(Y.T)            
+    
+    colors = cmocean.cm.balance(np.linspace(0.05,0.95,n)) 
+    hexcolors = [ "#{:02x}{:02x}{:02x}".format(int(colors[i][0]*255),int(colors[i][1]*255),int(colors[i][2]*255)) for i in range(len(colors)) ]
+    cmap = hexcolors
+
+    lat = [df[df['stationcode']==stationcode[value]]['stationlat'].iloc[0]]
+    lon = [-df[df['stationcode']==stationcode[value]]['stationlon'].iloc[0]]
+    var = []
+    station = df[df['stationcode']==stationcode[value]]['stationinfo'].iloc[0]
+    data = df[df['stationcode']==stationcode[value]].iloc[0]
+    
+#    fig = go.Figure(go.Densitymapbox(lat=lat, lon=lon, z=var, radius=10))
+    fig = go.Figure(px.scatter_mapbox(lat=lat, lon=lon, color_discrete_sequence=["darkred"], zoom=1))
+    fig.update_layout(mapbox_style="carto-positron", mapbox_center_lat=lat[0], mapbox_center_lon=lon[0]) 
+#    fig.update_layout(mapbox_style="stamen-watercolor", mapbox_center_lat=lat[0], mapbox_center_lon=lon[0]) 
+#    fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lat=lat[0], mapbox_center_lon=lon[0]) 
+#    fig.update_layout(title={'text': 'Location', 'x':0.5, 'y':0.925, 'xanchor': 'center', 'yanchor': 'top'})
+    fig.update_layout(height=200, width=600, margin={"r":80,"t":0,"l":40,"b":0})
     
     return fig
 
