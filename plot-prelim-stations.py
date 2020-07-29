e3bdcc9bc81f0deb8b03c29eb49c0f249d8e94ad
patternizer <patternizer@gmail.com>
2020-07-29 23:29:00 +0100

    Convert longitudes from +W to +E. New Delta CC regional histograms for Europe and SH (0-60S) excluding Australia. Histograms binned at 0.02 degrees C.

--- plot-prelim-stations.py
+++ plot-prelim-stations.py
@@ -4,8 +4,8 @@
 #------------------------------------------------------------------------------
 # PROGRAM: plot-prelim-stations.py
 #------------------------------------------------------------------------------
-# Version 0.5
-# 24 July, 2020
+# Version 0.6
+# 27 July, 2020
 # Michael Taylor
 # https://patternizer.github.io
 # patternizer AT gmail DOT com
@@ -158,10 +158,13 @@ def load_dataframe(filename_txt):
     f.close
 
     # construct dataframe
+    
     df = pd.DataFrame(columns=['year','1','2','3','4','5','6','7','8','9','10','11','12'])
     df['year'] = yearlist
+
     for j in range(1,13):
         df[df.columns[j]] = [ monthlist[i][j-1] for i in range(len(monthlist)) ]
+
     df['stationcode'] = stationcode    
     df['stationlat'] = stationlat
     df['stationlon'] = stationlon
@@ -175,6 +178,16 @@ def load_dataframe(filename_txt):
     df['stationcruindex'] = stationcruindex
     df['stationgridcell'] = stationgridcell
 
+    # trim strings
+    
+    df['stationcode'] = [ str(df['stationcode'][i]).strip() for i in range(len(df)) ] 
+    df['stationname'] = [ str(df['stationname'][i]).strip() for i in range(len(df)) ] 
+    df['stationcountry'] = [ str(df['stationcountry'][i]).strip() for i in range(len(df)) ] 
+    df['stationcruindex'] = [ str(df['stationcruindex'][i]).strip() for i in range(len(df)) ] 
+    df['stationgridcell'] = [ str(df['stationgridcell'][i]).strip() for i in range(len(df)) ] 
+
+    # convert numeric types to int
+
     df['stationlat'] = df['stationlat'].astype('int')
     df['stationlon'] = df['stationlon'].astype('int')
     df['stationelevation'] = df['stationelevation'].astype('int')
@@ -182,28 +195,29 @@ def load_dataframe(filename_txt):
     df['stationlastyear'] = df['stationlastyear'].astype('int')    
     df['stationsource'] = df['stationsource'].astype('int')    
     df['stationfirstreliable'] = df['stationfirstreliable'].astype('int')
-
-    # bad data handling
-
-    df['stationheader'] = stationheader          
-    nchar = [ len(df['stationheader'][i]) for i in range(len(df)) ]      
-    nwords = [ len(df['stationheader'][i].split()) for i in range(len(df)) ]      
-
 #    df['stationcruindex'] = df['stationcruindex'].astype('int')
 #    df['stationgridcell'] = df['stationgridcell'].astype('int') 
 
+    # bad data handling
+
+#    df['stationheader'] = stationheader          
+#    nchar = [ len(df['stationheader'][i]) for i in range(len(df)) ]      
+#    nwords = [ len(df['stationheader'][i].split()) for i in range(len(df)) ]      
+
 #    for i in range(len(df)):        
-#        if str(df['stationcruindex'][i]).strip()[1:].isdigit():
-#            continue
-#        else:
+#        if str(df['stationcruindex'][i])[1:].isdigit() == False:
 #            df['stationcruindex'][i] = np.nan
-#    [ df['stationlat'].replace(df['stationlat'][i], np.nan, inplace=True) for i in range(len(df)) if not df['stationlat'][i].strip()[1:].isdigit() ]
+#        else:
+#            continue
  
     # replace fillvalues
 
     df['year'].replace(-999, np.nan, inplace=True) 
+ 
     for j in range(1,13):
+    
         df[df.columns[j]].replace(-999, np.nan, inplace=True)
+
     df['stationlat'].replace(-999, np.nan, inplace=True) 
     df['stationlon'].replace(-9999, np.nan, inplace=True) 
     df['stationelevation'].replace(-999, np.nan, inplace=True) 
@@ -212,8 +226,8 @@ def load_dataframe(filename_txt):
     df['stationsource'].replace(-999, np.nan, inplace=True) 
     df['stationfirstreliable'].replace(-999, np.nan, inplace=True) 
 
-
     df.to_csv('df.csv')
+    
     return df
 
 #------------------------------------------------------------------------------
@@ -234,24 +248,18 @@ else:
     filename_txt = 'stat4.CRUTEM5.1prelim01.1721-2019.txt'
     df = load_dataframe(filename_txt)
 
-#------------------------------------------------------------------------------
-# APPLY SCALE FACTORS
-#------------------------------------------------------------------------------
-
-df = pd.read_csv('df.csv', index_col=0)
-df['stationlat'] = df['stationlat']/10.0
-df['stationlon'] = df['stationlon']/10.0
-for j in range(1,12+1):
-    df[df.columns[j]] = df[df.columns[j]]/100.0
-
-lat_min = df['stationlat'].min()
-lat_max = df['stationlat'].max()
-lon_min = df['stationlon'].min()
-lon_max = df['stationlon'].max()
-year_min = df['year'].min()
-year_max = df['year'].max()
-
-df.to_csv('df.csv')
+    #------------------------------------------------------------------------------
+    # APPLY SCALE FACTORS
+    #------------------------------------------------------------------------------
+    
+    df = pd.read_csv('df.csv', index_col=0)
+    df['stationlat'] = df['stationlat']/10.0
+    df['stationlon'] = df['stationlon']/10.0
+    for j in range(1,12+1):
+        df[df.columns[j]] = df[df.columns[j]]/100.0
+    # Convert +W to +E
+    df['stationlon'] = -df['stationlon']        
+    df.to_csv('df.csv')
 
 #------------------------------------------------------------------------------
 # BAD DATA ANALYSIS
@@ -279,63 +287,149 @@ df.to_csv('df.csv')
 # ['999099-999-9999-9999', 'LONDON', 'AUSTRALIA', '18971899', '101897', '0']
 # ['999216-999-9999-9999', 'LAS', 'DELICIAS', 'ARGENTINA', '19251964', '101925', '0']
 
+lat_min = df['stationlat'].min()
+lat_max = df['stationlat'].max()
+lon_min = df['stationlon'].min()
+lon_max = df['stationlon'].max()
+year_min = df['year'].min()
+year_max = df['year'].max()
+
 if plot_delta_cc == True:
 
     # Delta CC +/- 30 years around median value (data to 1900) = 1885
     
     nbins = 1900 - year_min + 1
     bins = np.linspace(year_min, 1900, nbins) 
-    counts, edges = np.histogram(df['year'], nbins, range=[year_min,1900+1], density=False)    
-    
+    counts, edges = np.histogram(df['year'], nbins, range=[year_min,1900+1], density=False)        
     year_Q2 = pd.Series(bins).iloc[(pd.Series(np.cumsum(counts))-np.sum(counts)*0.50).abs().argsort()[:1]].values[0]
     year_lo = year_Q2-30
     year_hi = year_Q2+30
+    
+    def plot_hist_array(diff, figstr, titlestr):
+        
+        def plot_hist(diff,i,row,col):
+            
+            nbins = int((0.5-(-0.5))/0.02) + 1
+            bins = np.linspace(-0.5, 0.5, nbins) 
+            counts, edges = np.histogram(diff[str(i)], nbins, range=[-0.5,0.5+0.02], density=False)    
+            Q1 = pd.Series(bins).iloc[(pd.Series(np.cumsum(counts))-np.sum(counts)*0.25).abs().argsort()[:1]].values[0]
+            Q2 = pd.Series(bins).iloc[(pd.Series(np.cumsum(counts))-np.sum(counts)*0.50).abs().argsort()[:1]].values[0]
+            Q3 = pd.Series(bins).iloc[(pd.Series(np.cumsum(counts))-np.sum(counts)*0.75).abs().argsort()[:1]].values[0]
+            axs[row,col].fill_between(bins, counts, step="post", facecolor='lightgrey', alpha=0.5)
+            axs[row,col].plot(bins, counts, drawstyle='steps-post', linewidth=1, color='black')    
+            axs[row,col].axvline(x=Q1, color='blue', label='Q1: ' + "{0:.2f}".format(Q1))   
+            axs[row,col].axvline(x=Q2, color='red', label='Q2: ' + "{0:.2f}".format(Q2))    
+            axs[row,col].axvline(x=Q3, color='blue', label='Q3: ' + "{0:.2f}".format(Q3))    
+            axs[row,col].set_title('Month=' + "{0:.0f}".format(i), fontsize=15)
+        
+        fig, axs = plt.subplots(4,3,figsize=(15,10))
+        plot_hist(diff,1,0,0)
+        plot_hist(diff,2,0,1)
+        plot_hist(diff,3,0,2)
+        plot_hist(diff,4,1,0)
+        plot_hist(diff,5,1,1)
+        plot_hist(diff,6,1,2)
+        plot_hist(diff,7,2,0)
+        plot_hist(diff,8,2,1)
+        plot_hist(diff,9,2,2)
+        plot_hist(diff,10,3,0)
+        plot_hist(diff,11,3,1)
+        plot_hist(diff,12,3,2)
+        for ax in axs.flat:
+            ax.set_xlabel("$\mathrm{\Delta}$(anomaly), $\mathrm{\degree}$C", fontsize=15)
+            ax.set_ylabel("Counts / 0.02$\mathrm{\degree}$C", fontsize=15)
+        for ax in axs.flat:
+            ax.label_outer()
+            ax.legend(loc='best', fontsize=8)   
+        
+        fig.suptitle(titlestr, fontsize=fontsize)    
+        plt.savefig(figstr)
+        plt.close(fig)
 
-    delta_lo = df[(df['year']>=year_lo) & (df['year']<year_Q2)].groupby(['stationcode']).mean()
-    delta_hi = df[(df['year']>=year_Q2) & (df['year']<year_hi)].groupby(['stationcode']).mean()
-    diff = delta_hi - delta_lo
-    
-    def plot_hist(diff,i,row,col):
+    delta_lo_60N90N = df[(df['year']>=year_lo) & (df['year']<year_Q2) & (df['stationlat']>=60) & (df['stationlat']<90)].groupby(['stationcode']).mean()
+    delta_hi_60N90N = df[(df['year']>=year_Q2) & (df['year']<year_hi) & (df['stationlat']>=60) & (df['stationlat']<90)].groupby(['stationcode']).mean()
+    diff = delta_hi_60N90N - delta_lo_60N90N    
+    figstr = 'delta_cc_60N90N.png'
+    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885: 60-90°N'
+    plot_hist_array(diff, figstr, titlestr)
+
+    delta_lo_30N60N = df[(df['year']>=year_lo) & (df['year']<year_Q2) & (df['stationlat']>=30) & (df['stationlat']<60)].groupby(['stationcode']).mean()
+    delta_hi_30N60N = df[(df['year']>=year_Q2) & (df['year']<year_hi) & (df['stationlat']>=30) & (df['stationlat']<60)].groupby(['stationcode']).mean()
+    diff = delta_hi_30N60N - delta_lo_30N60N    
+    figstr = 'delta_cc_30N60N.png'
+    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885: 30-60°N'
+    plot_hist_array(diff, figstr, titlestr)
+
+    delta_lo_00N30N = df[(df['year']>=year_lo) & (df['year']<year_Q2) & (df['stationlat']>=0) & (df['stationlat']<30)].groupby(['stationcode']).mean()
+    delta_hi_00N30N = df[(df['year']>=year_Q2) & (df['year']<year_hi) & (df['stationlat']>=0) & (df['stationlat']<30)].groupby(['stationcode']).mean()
+    diff = delta_hi_00N30N - delta_lo_00N30N    
+    figstr = 'delta_cc_00N30N.png'
+    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885: 0-30°N'
+    plot_hist_array(diff, figstr, titlestr)
+
+    delta_lo_00S30S = df[(df['year']>=year_lo) & (df['year']<year_Q2) & (df['stationlat']<=0) & (df['stationlat']>-30)].groupby(['stationcode']).mean()
+    delta_hi_00S30S = df[(df['year']>=year_Q2) & (df['year']<year_hi) & (df['stationlat']<=0) & (df['stationlat']>-30)].groupby(['stationcode']).mean()
+    diff = delta_hi_00S30S - delta_lo_00S30S    
+    figstr = 'delta_cc_00S30S.png'
+    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885: 0-30°S'
+    plot_hist_array(diff, figstr, titlestr)
+
+    delta_lo_30S60S = df[(df['year']>=year_lo) & (df['year']<year_Q2) & (df['stationlat']<=-30) & (df['stationlat']>-60)].groupby(['stationcode']).mean()
+    delta_hi_30S60S = df[(df['year']>=year_Q2) & (df['year']<year_hi) & (df['stationlat']<=-30) & (df['stationlat']>-60)].groupby(['stationcode']).mean()
+    diff = delta_hi_30S60S - delta_lo_30S60S    
+    figstr = 'delta_cc_30S60S.png'
+    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885: 30-60°S'
+    plot_hist_array(diff, figstr, titlestr)
+
+    delta_lo_60S90S = df[(df['year']>=year_lo) & (df['year']<year_Q2) & (df['stationlat']<=-60) & (df['stationlat']>-90)].groupby(['stationcode']).mean()
+    delta_hi_60S90S = df[(df['year']>=year_Q2) & (df['year']<year_hi) & (df['stationlat']<=-60) & (df['stationlat']>-90)].groupby(['stationcode']).mean()
+    diff = delta_hi_60S90S - delta_lo_60S90S    
+    figstr = 'delta_cc_60S90S.png'
+    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885: 60-90°S'
+    plot_hist_array(diff, figstr, titlestr)
+
+    delta_lo_global = df[(df['year']>=year_lo) & (df['year']<year_Q2)].groupby(['stationcode']).mean()
+    delta_hi_global = df[(df['year']>=year_Q2) & (df['year']<year_hi)].groupby(['stationcode']).mean()
+    diff = delta_hi_global - delta_lo_global    
+    figstr = 'delta_cc_global.png'
+    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885: Global'
+    plot_hist_array(diff, figstr, titlestr)
+
+    delta_lo_us = df[(df['year']>=year_lo) & (df['year']<year_Q2) & (df['stationcountry']=='USA')].groupby(['stationcode']).mean()
+    delta_hi_us = df[(df['year']>=year_Q2) & (df['year']<year_hi) & (df['stationcountry']=='USA')].groupby(['stationcode']).mean()
+    diff = delta_hi_us - delta_lo_us    
+    figstr = 'delta_cc_us.png'
+    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885: USA'
+    plot_hist_array(diff, figstr, titlestr)
+
+    delta_lo_australia = df[(df['year']>=year_lo) & (df['year']<year_Q2) & (df['stationcountry']=='AUSTRALIA')].groupby(['stationcode']).mean()
+    delta_hi_australia = df[(df['year']>=year_Q2) & (df['year']<year_hi) & (df['stationcountry']=='AUSTRALIA')].groupby(['stationcode']).mean()
+    diff = delta_hi_australia - delta_lo_australia   
+    figstr = 'delta_cc_australia.png'
+    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885: Australia'
+    plot_hist_array(diff, figstr, titlestr)
+
+    delta_lo_00S60S_noaus = df[(df['year']>=year_lo) & (df['year']<year_Q2) & (df['stationlat']<=0) & (df['stationlat']>-60) & (df['stationcountry']!='AUSTRALIA')].groupby(['stationcode']).mean()
+    delta_hi_00S60S_noaus = df[(df['year']>=year_Q2) & (df['year']<year_hi) & (df['stationlat']<=0) & (df['stationlat']>-60) & (df['stationcountry']!='AUSTRALIA')].groupby(['stationcode']).mean()
+    diff = delta_hi_00S60S_noaus - delta_lo_00S60S_noaus    
+    figstr = 'delta_cc_00S60S_noaus.png'
+    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885: 0-60°S (-Australia)'
+    plot_hist_array(diff, figstr, titlestr)
         
-        nbins = int((0.5-(-0.5))/0.01) + 1
-        bins = np.linspace(-0.5, 0.5, nbins) 
-        counts, edges = np.histogram(diff[str(i)], nbins, range=[-0.5,0.5+0.01], density=False)    
-        Q1 = pd.Series(bins).iloc[(pd.Series(np.cumsum(counts))-np.sum(counts)*0.25).abs().argsort()[:1]].values[0]
-        Q2 = pd.Series(bins).iloc[(pd.Series(np.cumsum(counts))-np.sum(counts)*0.50).abs().argsort()[:1]].values[0]
-        Q3 = pd.Series(bins).iloc[(pd.Series(np.cumsum(counts))-np.sum(counts)*0.75).abs().argsort()[:1]].values[0]
-        axs[row,col].fill_between(bins, counts, step="post", facecolor='lightgrey', alpha=0.5)
-        axs[row,col].plot(bins, counts, drawstyle='steps-post', linewidth=1, color='black')    
-        axs[row,col].axvline(x=Q1, color='blue', label='Q1: ' + "{0:.2f}".format(Q1))   
-        axs[row,col].axvline(x=Q2, color='red', label='Q2: ' + "{0:.2f}".format(Q2))    
-        axs[row,col].axvline(x=Q3, color='blue', label='Q3: ' + "{0:.2f}".format(Q3))    
-        axs[row,col].set_title('Month=' + "{0:.0f}".format(i), fontsize=15)
-    
-    fig, axs = plt.subplots(4,3,figsize=(15,10))
-    plot_hist(diff,1,0,0)
-    plot_hist(diff,2,0,1)
-    plot_hist(diff,3,0,2)
-    plot_hist(diff,4,1,0)
-    plot_hist(diff,5,1,1)
-    plot_hist(diff,6,1,2)
-    plot_hist(diff,7,2,0)
-    plot_hist(diff,8,2,1)
-    plot_hist(diff,9,2,2)
-    plot_hist(diff,10,3,0)
-    plot_hist(diff,11,3,1)
-    plot_hist(diff,12,3,2)
-    for ax in axs.flat:
-        ax.set_xlabel("$\mathrm{\Delta}$(anomaly), $\mathrm{\degree}$C", fontsize=15)
-        ax.set_ylabel("Counts / 0.01$\mathrm{\degree}$C", fontsize=15)
-    for ax in axs.flat:
-        ax.label_outer()
-        ax.legend(loc='best', fontsize=8)   
-    
-    figstr = 'delta_cc.png'
-    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885'
-    fig.suptitle(titlestr, fontsize=fontsize)    
-    plt.savefig(figstr)
-    plt.close(fig)
-        
+    delta_lo_eu = df[(df['year']>=year_lo) & (df['year']<year_Q2) & (df['stationlat']>=30) & (df['stationlat']<70) & (df['stationlon']>=-10) & (df['stationlon']<50) ].groupby(['stationcode']).mean()
+    delta_hi_eu = df[(df['year']>=year_Q2) & (df['year']<year_hi) & (df['stationlat']>=30) & (df['stationlat']<70) & (df['stationlon']>=-10) & (df['stationlon']<50) ].groupby(['stationcode']).mean()
+    diff = delta_hi_eu - delta_lo_eu    
+    figstr = 'delta_cc_eu.png'
+    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885: Europe + ME'
+    plot_hist_array(diff, figstr, titlestr)
+
+    delta_lo_eu2 = df[(df['year']>=year_lo) & (df['year']<year_Q2) & (df['stationlat']>=30) & (df['stationlat']<70) & (df['stationlon']>=-10) & (df['stationlon']<30) ].groupby(['stationcode']).mean()
+    delta_hi_eu2 = df[(df['year']>=year_Q2) & (df['year']<year_hi) & (df['stationlat']>=30) & (df['stationlat']<70) & (df['stationlon']>=-10) & (df['stationlon']<30) ].groupby(['stationcode']).mean()
+    diff = delta_hi_eu2 - delta_lo_eu2    
+    figstr = 'delta_cc_eu2.png'
+    titlestr = 'Histograms of mean change in anomaly $\mathrm{\pm}$ 30 years of 1885: Europe (-ME)'
+    plot_hist_array(diff, figstr, titlestr)
+
     #for i in range(1,13):
     
     #    counts, edges = np.histogram(diff[str(i)], nbins, range=[-1.0,1.0+0.01], density=False)    
@@ -623,7 +717,7 @@ if plot_locations == True:
     
     # PLOT: quick plot of stations on world map
 
-    lon = -df['stationlon']
+    lon = df['stationlon']
     lat = df['stationlat']
     
     figstr = 'location_map.png'
