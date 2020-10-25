3ff23f65e8a30bc0fad901ee3c6a995e9a2092cd
Michael Taylor <patternizer@gmail.com>
2020-10-25 19:17:58 +0000

    Remove app tab title that causes heroku deploy fail

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #------------------------------------------------------------------------------
-# PROGRAM: app.py
+# PROGRAM: app.py (multipage)
 #------------------------------------------------------------------------------
 # Version 0.1
 # 25 October, 2020
@@ -13,7 +13,8 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 
 external_stylesheets=[dbc.themes.DARKLY]
-app = dash.Dash(__name__, title='GloSAT Station Viewer', update_title='Loading...', external_stylesheets=external_stylesheets)
+#app = dash.Dash(__name__, title='GloSAT Station Viewer', update_title='Loading...', external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.config.suppress_callback_exceptions = True
 
