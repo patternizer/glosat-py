6518dbb200df80779ae59ce75d4e2d50c46ab936
Michael Taylor <patternizer@gmail.com>
2020-10-31 00:40:08 +0000

    Added about page to multipage app

--- apps/about.py
+++ apps/about.py
@@ -14,46 +14,164 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
+plot_stripes = 'url(/assets/plot-stripes.png)'
+plot_timeseries = 'url(/assets/plot-timeseries.png)'
+plot_climatology = 'url(/assets/plot-climatology.png)'
+plot_ranks = 'url(/assets/plot-ranks.png)'
+plot_spiral = 'url(/assets/plot-spiral.png)'
+plot_fry = 'url(/assets/emily-stations-fry.png)'
+
+
+faq_1 = [
+    dbc.CardHeader("FAQ #1"),
+    dbc.CardImg(src='assets/emily-stations-fry.png', top=True, style={'height':'10vh'}),
+    dbc.CardBody(
+        [html.H5("First Reliable Years", className="card-title"),
+        html.P(
+        "How are they calculated?",
+        className="card-text")],
+    )]
+
+faq_2 = [
+    dbc.CardHeader("FAQ #2"),
+    dbc.CardImg(src='assets/plot-stripes.png', top=True, style={'height':'10vh'}),
+    dbc.CardBody(
+        [html.H5("Climate Stripes", className="card-title"),
+        html.P(
+        "How are they calculated?",
+        className="card-text")],
+    )]
+
+faq_3 = [
+    dbc.CardHeader("FAQ #3"),
+    dbc.CardImg(src='assets/plot-timeseries.png', top=True, style={'height':'10vh'}),
+    dbc.CardBody(
+        [html.H5("Anomaly Timeseries", className="card-title"),
+        html.P(
+        "How are they calculated?",
+        className="card-text")],
+    )]
+
+faq_4 = [
+    dbc.CardHeader("FAQ #4"),
+    dbc.CardImg(src='assets/plot-climatology.png', top=True, style={'height':'10vh'}),
+    dbc.CardBody(
+        [html.H5("Monthly Climatology", className="card-title"),
+        html.P(
+        "How is it calculated?",
+        className="card-text")],
+    )]
+
+faq_5 = [
+    dbc.CardHeader("FAQ #5"),
+    dbc.CardImg(src='assets/plot-ranks.png', top=True, style={'height':'10vh'}),
+    dbc.CardBody(
+        [html.H5("Rank Distribution", className="card-title"),
+        html.P(
+        "How is it calculated?",
+        className="card-text")],
+    )]
+
+faq_6 = [
+    dbc.CardHeader("FAQ #6"),
+    dbc.CardImg(src='assets/plot-spiral.png', top=True, style={'height':'10vh'}),
+    dbc.CardBody(
+        [html.H5("Warming Spiral", className="card-title"),
+        html.P(
+        "How is it calculated?",
+        className="card-text")],
+    )]
+
 layout = html.Div([
     dbc.Container([
-        dbc.Row([
 
+        dbc.Card(                    
+            children=[
+                html.P([
+                    html.H3(children=dbc.Button("Station Viewer App", href="/glosat", color="primary", className="mt-2"), className="text-center"),                              
+                ], className="text-center"),
+                html.Label(['Michael Taylor¹, Phil Jones¹, Tim Osborn¹, David Lister¹, Ian Harris¹, Emily Wallis¹'], className="text-center"),
+                html.P([html.A('¹Climatic Research Unit', href='http://www.cru.uea.ac.uk'), ', School of Environmental Sciences, University of East Anglia'], className="text-center"),
+                html.P(['This research was partly funded by the ', html.A('GloSAT Project', href='https://www.glosat.org/'), '.'], className="text-center"),  
+            ], body=True, color="dark", outline=True), 
+            
+        html.Br(),
+              
+        dbc.Row([     
             dbc.Col(
                 dbc.Card(
-                    
-                    children=[html.H3(children='The App', className="text-center"),                        
-                    html.P("Developed by:"),
-                    html.Label(['Phil Jones¹, Tim Osborn¹, David Lister¹, Ian Harris¹, Emily Wallis¹, Michael Taylor¹']),
-                    html.P('¹Climatic Research Unit, School of Environmental Sciences, University of East Anglia'),
-                    html.P('This research was partly funded by the GloSAT Project.'),
-                    html.H4(children='License', className="text-left"),
-                    html.Label(['The results, maps and figures shown on this website are licenced under an ', html.A('Open Government License', href='http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/'), '.']),                            
-                ], body=True, color="dark", outline=True), width=4, className="mb-4"),
-             
+                    children=[
+                        html.H5(children='GloSAT Dataset', className="text-center"),
+                        dbc.Row([
+                            dbc.Col(
+                                dbc.Button("Station Data", href="http://crudata.uea.ac.uk/cru/data/temperature/crutem4/station-data.htm", color="primary"),
+                                className="mt-2", align="center"),
+                            dbc.Col(
+                                dbc.Button("Temperatures", href="http://crudata.uea.ac.uk/cru/data/temperature/", color="primary"),
+                               className="mt-2", align="center"),
+                        ], justify="center")
+                    ],
+                body=True, color="dark", outline=True),
+            ),
             dbc.Col(
-                dbc.Card(children=[html.H3(children='Privacy Policy', className="text-center"),                                   
-                    html.P("© UEA All rights reserved"),
-                    html.P('This website has been created for the GloSAT Project and does not use privately hosted web-analytics tools, does not store cookies and respects do not track settings.'),
-                    html.P('For further information please refer to the UEA privacy policy.'),                    
-                    dbc.Button("Policy", href="https://www.uea.ac.uk/about/university-information/statutory-legal-policies", color="primary", className="mt-2"),
-                ], body=True, color="dark", outline=True), width=4, className="mb-2"),
-
+                dbc.Card(
+                    children=[
+                        html.H5(children='Project Website', className="text-center"),
+                        dbc.Button("GloSAT", href="https://www.glosat.org", color="primary", className="mt-2"),
+                    ],
+                body=True, color="dark", outline=True),
+            ),
             dbc.Col(
-                dbc.Card(children=[html.H3(children='Contact', className="text-center"),
-                    html.P('For any enquiries please contact:'),
-                    html.A('Climatic Research Unit (CRU)'),
-                    html.A('School of Environmental Sciences'),
-                    html.A('Faculty of Science'),
-                    html.A('University of East Anglia'),
-                    html.A('Norwich Research Park'),
-                    html.A('Norwich NR4 7TJ, UK.'),
-                    html.P(html.Br()),
-                    html.A('Telephone: +44 (0) 1603 592542'),
-                    html.A('Fax: +44 (0) 1603 591327'),    
-                    dbc.Button("Email", href="cru@uea.ac.uk", color="primary", className="mt-2"),
-                ], body=True, color="dark", outline=True), width=4, className="mb-2"),
+                dbc.Card(
+                    children=[
+                        html.H5(children='Plotly Code', className="text-center"),
+                        dbc.Button("GitHub", href="https://github.com/patternizer/glosat-py", color="primary", className="mt-2"),
+                    ],
+                body=True, color="dark", outline=True),
+            ),            
+        ], className="mb-4"),
+        
+        html.Div(children=
+            [
+            dbc.Row(
+                [
+                dbc.Col(dbc.Card(faq_1, color="secondary", inverse=True)),
+                dbc.Col(dbc.Card(faq_2, color="light", inverse=True)),
+                dbc.Col(dbc.Card(faq_3, color="danger", inverse=True)),
+                dbc.Col(dbc.Card(faq_4, color="info", inverse=True)),
+                dbc.Col(dbc.Card(faq_5, color="success", inverse=True)),
+                dbc.Col(dbc.Card(faq_6, color="warning", inverse=True)),
+#               dbc.Col(dbc.Card(card_content, color="primary")),
+#               dbc.Col(dbc.Card(card_content, color="dark", inverse=True)),
+                ], className="mb-4"),
+            ]
+        ),
 
-        ], className="mb-2"),
-    ])
+        dbc.CardDeck(
+            [             
+            dbc.Card(children=[html.H4(children='License', className="text-center"), 
+                html.P(html.Br()),                               
+                html.P("© UEA all rights reserved"),
+                html.Label(['The results, maps and figures shown on this website are licenced under an ', html.A('Open Government License', href='http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/'), '.']),                                                    
+                html.P('This website has been created for the GloSAT Project and does not use privately hosted web-analytics tools, does not store cookies and respects do not track settings.'),
+                html.P('For further information please refer to the UEA privacy policy.'),                    
+                dbc.Button("UEA Privacy Policy", href="https://www.uea.ac.uk/about/university-information/statutory-legal-policies", color="primary", className="mt-2"),
+                ], body=True, color="dark", outline=True),                 
+            dbc.Card(children=[html.H4(children='Contact', className="text-center"),
+                html.P(html.Br()),                               
+                html.P('For any enquiries please contact:'),
+                html.A('Climatic Research Unit (CRU)'),
+                html.A('School of Environmental Sciences'),
+                html.A('Faculty of Science'),
+                html.A('University of East Anglia'),
+                html.A('Norwich Research Park'),
+                html.A('Norwich NR4 7TJ, UK.'),
+                html.P(html.Br()),
+                html.A('Telephone: +44 (0) 1603 592542'),
+                html.A('Email: cru@uea.ac.uk'),
+                ], body=True, color="dark", outline=True),                 
+            ], className="mb-2"),
+
+    ], className="mb-2"),
 ])
 
--- apps/home.py
+++ apps/home.py
@@ -15,64 +15,65 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 
 image = 'url(/assets/emily-stations-fry.png)'
-layout = html.Div([
 
+layout = html.Div([
     dbc.Container([
 
         dbc.Row([
             dbc.Card(
                 children=[
-                    dbc.CardImg(src='assets/emily-stations-fry.png', top=False, style={'height':'50vh'}),
+                    dbc.CardImg(src='assets/emily-stations-fry.png', top=False, style={'height':'70vh'}),
                     dbc.CardBody(
-                        dbc.Button("App", 
-                            href="/glosat", 
-                            color="primary"), className="text-center",
+                        dbc.Button("Station Viewer App", href="/glosat", color="primary"), 
+                        className="text-center",
                     ),
                 ], 
             body=False, color="dark", outline=True), 
         ], justify="center"),
+
         html.Br(),
 
         dbc.Row([
-            dbc.Col(dbc.Card(children=[html.H3(children='GloSATp02 Dataset',
-                                               className="text-center"),
-                                       dbc.Row([dbc.Col(dbc.Button("GloSATp02", href="http://crudata.uea.ac.uk/cru/data/temperature/crutem4/station-data.htm",
-                                                                   color="primary"),
-                                                        className="mt-2"),
-                                                dbc.Col(dbc.Button("Descriptor", href="http://crudata.uea.ac.uk/cru/data/temperature/",
-                                                                   color="primary"),
-                                                        className="mt-2")], justify="center")
-                                       ],
-                             body=True, color="dark", outline=True)
-                    , width=4, className="mb-4"),
+            dbc.Col(
+                dbc.Card(
+                    children=[
+                        html.H5(children='GloSAT Dataset', className="text-center"),
+                        dbc.Row([
+                            dbc.Col(
+                                dbc.Button("Station Data", href="http://crudata.uea.ac.uk/cru/data/temperature/crutem4/station-data.htm", color="primary"),
+                                className="mt-2", align="center"),
+                            dbc.Col(dbc.Button("Temperatures", href="http://crudata.uea.ac.uk/cru/data/temperature/", color="primary"),
+                                className="mt-2", align="center"),
+                        ], justify="center")
+                    ],
+                body=True, color="dark", outline=True),
+            width=4, className="mb-4"),
 
-            dbc.Col(dbc.Card(children=[html.H3(children='GloSAT project',
-                                               className="text-center"),
-                                       dbc.Button("GloSAT",
-                                                  href="https://www.glosat.org",
-                                                  color="primary",
-                                                  className="mt-2"),
+            dbc.Col(
+                dbc.Card(
+                    children=[
+                        html.H5(children='Project Website', className="text-center"),
+                        dbc.Button("GloSAT", href="https://www.glosat.org", color="primary", className="mt-2"),
+                    ],
+                body=True, color="dark", outline=True),
+            width=4, className="mb-2"),
 
-                                       ],
-                             body=True, color="dark", outline=True)
-                    , width=4, className="mb-2"),
-
-            dbc.Col(dbc.Card(children=[html.H3(children='Plotly Code',
-                                               className="text-center"),
-                                       dbc.Button("GitHub",
-                                                  href="https://github.com/patternizer/glosat-py",
-                                                  color="primary",
-                                                  className="mt-2"),
-                                       ],
-                             body=True, color="dark", outline=True)
-                    , width=4, className="mb-2"),
-
+            dbc.Col(
+                dbc.Card(
+                    children=[
+                        html.H5(children='Plotly Code', className="text-center"),
+                        dbc.Button("GitHub", href="https://github.com/patternizer/glosat-py", color="primary", className="mt-2"),
+                    ],
+                body=True, color="dark", outline=True),
+            width=4, className="mb-2"),
+            
         ], className="mb-2"),
 
-        dbc.Row([
-        html.A("GloSAT Station Viewer is brought to you by the Climatic Research Unit at the University of East Anglia",
-               href="http://www.cru.uea.ac.uk/")
-        ], className="mb-8"),
+        html.P(['GloSAT Station Viewer is brought to you by the ', html.A('Climatic Research Unit', href='http://www.cru.uea.ac.uk'), ' in the School of Environmental Sciences, University of East Anglia']),
+
+#        dbc.Row([
+#            html.A("GloSAT Station Viewer is brought to you by the Climatic Research Unit at the University of East Anglia", href="http://www.cru.uea.ac.uk/"),
+#        ], className="mb-8"),
 
     ])
 ])
