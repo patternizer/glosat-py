b982b274851e6f752591704d0d1b4f0dab9d98e4
patternizer <michael.a.taylor@uea.ac.uk>
2022-06-10 00:59:32 +0100

    Updated all pages to new Dash version migration requirements

--- app.py
+++ app.py
@@ -8,11 +8,13 @@
 # patternizer AT gmail DOT com
 #------------------------------------------------------------------------------
 
-import dash
 import flask
-import dash_core_components as dcc
+import dash
+from dash import dcc
+from dash import html
+#import dash_core_components as dcc
+#import dash_html_components as html
 import dash_bootstrap_components as dbc
-import dash_html_components as html
 
 external_stylesheets=[dbc.themes.DARKLY]
 #app = dash.Dash(__name__, title='GloSAT Station Viewer', update_title='Loading...', external_stylesheets=external_stylesheets)
