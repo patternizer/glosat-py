1ba22574fc0763285dba87ac51670d885d323797
patternizer <michael.a.taylor@uea.ac.uk>
2022-06-18 11:17:37 +0100

    Rolling app.py to previous working version. Updated Heroku stack instead.

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 #------------------------------------------------------------------------------
 # PROGRAM: app.py (multipage)
 #------------------------------------------------------------------------------
-# Version 0.2
-# 18 June, 2022
+# Version 0.1
+# 25 October, 2020
 # Michael Taylor
 # https://patternizer.github.io
 # patternizer AT gmail DOT com
-# michael DOT a DOT taylor AT uea DOT ac DOT uk
 #------------------------------------------------------------------------------
 
+import flask
 import dash
+from dash import dcc
+from dash import html
+#import dash_core_components as dcc
+#import dash_html_components as html
 import dash_bootstrap_components as dbc
 
 external_stylesheets=[dbc.themes.DARKLY]
+#app = dash.Dash(__name__, title='GloSAT Station Viewer', update_title='Loading...', external_stylesheets=external_stylesheets)
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
+#app = dash.Dash(__name__, server=server)
+#server = flask.Flask(__name__)
 app.config.suppress_callback_exceptions = True
+
+
 
