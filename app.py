c7a6fc38611433c7a76765e73d770389991cc742
patternizer <michael.a.taylor@uea.ac.uk>
2022-06-18 11:09:51 +0100

    Removed flask import and added call as __main__

--- app.py
+++ app.py
@@ -13,9 +13,7 @@ import dash
 import dash_bootstrap_components as dbc
 
 external_stylesheets=[dbc.themes.DARKLY]
-app = dash.Dash(external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.config.suppress_callback_exceptions = True
 
-if __name__ == "__main__":
-    app.run_server()
